182240a3a901d0dab7b62fc30a397af16f532572
rebic <zhouyongchao@nnfe.com>
2016-11-22 18:57:33 +0800

    替换网站模版

--- src/main/resources/static/assets/js/ztree.js
+++ src/main/resources/static/assets/js/ztree.js
@@ -1,4 +1,4 @@
-var zTree,rMenu;
+var zTree;
 var demoIframe;
 
 var check_setting = {
@@ -39,88 +39,9 @@ var check_setting = {
             $("#normalMinValue").val(treeNode.normalMinValue);
             $("#normalMaxValue").val(treeNode.normalMaxValue);
             $("#unit").val(treeNode.unit);
-        },
-        beforeRightClick :function(){
-            rMenu = $("#rMenu");
-            return true;
-        },
-        onRightClick:function(event, treeId, treeNode) {
-            console.log("come in step 1...")
-            if (!treeNode && event.target.tagName.toLowerCase() != "button" && $(event.target).parents("a").length == 0) {
-                console.log("come in step 2...")
-                zTree.cancelSelectedNode();
-                showRMenu("root", event.clientX, event.clientY);
-            } else if (treeNode && !treeNode.noR) {
-                console.log("come in step 3...")
-                zTree.selectNode(treeNode);
-                showRMenu("node", event.clientX, event.clientY);
-            }
         }
-
     }
 };
-
-function showRMenu(type, x, y) {
-    $("#rMenu ul").show();
-    if (type=="root") {
-        $("#m_del").hide();
-        $("#m_check").hide();
-        $("#m_unCheck").hide();
-    } else {
-        $("#m_del").show();
-        $("#m_check").show();
-        $("#m_unCheck").show();
-    }
-    rMenu.css({"top":y+"px", "left":x+"px", "visibility":"visible"});
-
-    $("body").bind("mousedown", onBodyMouseDown);
-}
-
-function hideRMenu() {
-    if (rMenu) rMenu.css({"visibility": "hidden"});
-    $("body").unbind("mousedown", onBodyMouseDown);
-}
-function onBodyMouseDown(event){
-    if (!(event.target.id == "rMenu" || $(event.target).parents("#rMenu").length>0)) {
-        rMenu.css({"visibility" : "hidden"});
-    }
-}
-var addCount = 1;
-function addTreeNode() {
-    hideRMenu();
-    var newNode = { name:"newNode " + (addCount++)};
-    if (zTree.getSelectedNodes()[0]) {
-        newNode.checked = zTree.getSelectedNodes()[0].checked;
-        zTree.addNodes(zTree.getSelectedNodes()[0], newNode);
-    } else {
-        zTree.addNodes(null, newNode);
-    }
-}
-function removeTreeNode() {
-    hideRMenu();
-    var nodes = zTree.getSelectedNodes();
-    if (nodes && nodes.length>0) {
-        if (nodes[0].children && nodes[0].children.length > 0) {
-            var msg = "If you delete this node will be deleted along with sub-nodes. \n\nPlease confirm!";
-            if (confirm(msg)==true){
-                zTree.removeNode(nodes[0]);
-            }
-        } else {
-            zTree.removeNode(nodes[0]);
-        }
-    }
-}
-function checkTreeNode(checked) {
-    var nodes = zTree.getSelectedNodes();
-    if (nodes && nodes.length>0) {
-        zTree.checkNode(nodes[0], checked, true);
-    }
-    hideRMenu();
-}
-function resetTree() {
-    hideRMenu();
-    $.fn.zTree.init($("#treeDemo"), setting, zNodes);
-}
 
 
 function loadReady() {
